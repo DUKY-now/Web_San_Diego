86c5406c0ff520ba9d56e6656d7d89a55738e054
DUKY-now <dukynow@gmail.com>
2025-11-20 11:49:45 +0100

    controller faq, page moderation

---
+++ src/Entity/Members.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MembersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MembersRepository::class)]
+class Members
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Fonction = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Photo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getFonction(): ?string
+    {
+        return $this->Fonction;
+    }
+
+    public function setFonction(string $Fonction): static
+    {
+        $this->Fonction = $Fonction;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->Photo;
+    }
+
+    public function setPhoto(?string $Photo): static
+    {
+        $this->Photo = $Photo;
+
+        return $this;
+    }
+}
